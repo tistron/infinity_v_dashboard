b9233a696614b847e21351c014a4d5ac8485ac94
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 22:14:21 +0100

    Update VehiclesParkingCrudController.php

--- app/Http/Controllers/Admin/VehiclesParkingCrudController.php
+++ app/Http/Controllers/Admin/VehiclesParkingCrudController.php
@@ -45,8 +45,7 @@ class VehiclesParkingCrudController extends CrudController
     protected function setupListOperation()
     {
         $this->crud->removeButton('create');
-
-        CRUD::column('id')->type('text');
+        
         CRUD::column('model')->type('int');
         CRUD::column('status')->type('string');
         CRUD::column('tuning')->type('string');
