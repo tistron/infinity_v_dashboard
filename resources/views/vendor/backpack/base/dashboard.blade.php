d10d120f718cc6b9be04609d501aff3dacf2ff1d
Tim Strößner <ts@fp.de>
2022-12-21 16:18:49 +0100

    Update dashboard.blade.php

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -4,9 +4,6 @@
     $widgets['before_content'][] = [
         'type'        => 'jumbotron',
         'heading'     => trans('backpack::base.welcome'),
-        'content'     => trans('backpack::base.use_sidebar'),
-        'button_link' => backpack_url('logout'),
-        'button_text' => trans('backpack::base.logout'),
     ];
 @endphp
 
