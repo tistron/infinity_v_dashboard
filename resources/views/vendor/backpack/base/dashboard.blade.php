25aa7ad761dd05a64a1119a7d5b358da9fb587b0
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 19:17:13 +0100

    Update dashboard.blade.php

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends(backpack_view('blank'))
 
 @php
+
     use App\Http\Controllers\Admin\UsersCrudController;
 
     $widgets['before_content'][] = [
@@ -11,37 +12,21 @@
 
 
     $user_count = DB::connection('mysql_fivem')->table('users')->count();
+    $vehicle_count = DB::connection('mysql_fivem')->table('owned_vehicles')->count();
+    $instagram_posts = DB::connection('mysql_fivem')->table('instagram_posts')->count();
+
+    $info_body_text = 'Anzahl User: ' . $user_count;
+    $info_body_text .= '\n Anzahl Fahrzeuge: ' . $vehicle_count;
+
     $user_count_card_widget_definition = [
         'type'       => 'card',
         'class'      => 'card bg-dark text-white',
         'content'    => [
-            'header' => 'Anzahl der Userdatensätze',
-            'body'   => $user_count,
+            'header' => 'Interessante Daten zum Server',
+            'body'   => $info_body_text,
         ]
     ];
     Widget::add($user_count_card_widget_definition)->to('after_content');
-
-    $vehicle_count = DB::connection('mysql_fivem')->table('owned_vehicles')->count();
-    $vehicle_count_card_widget_definition = [
-        'type'       => 'card',
-        'class'      => 'card bg-dark text-white',
-        'content'    => [
-            'header' => 'Anzahl der Fahrzeuge',
-            'body'   => $vehicle_count,
-        ]
-    ];
-    Widget::add($vehicle_count_card_widget_definition)->to('after_content');
-
-    $instagram_posts = DB::connection('mysql_fivem')->table('instagram_posts')->count();
-    $instagram_post_count_card_widget_definition = [
-        'type'       => 'card',
-        'class'      => 'card bg-dark text-white',
-        'content'    => [
-            'header' => 'Anzahl der Instagram Posts',
-            'body'   => $instagram_posts,
-        ]
-    ];
-    Widget::add($instagram_post_count_card_widget_definition)->to('after_content');
 
 @endphp
 
