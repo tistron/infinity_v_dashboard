7971def0b38ca02bdaf02dc93979c5c7d2d01509
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 18:20:31 +0100

    Update dashboard.blade.php

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,11 +1,20 @@
 @extends(backpack_view('blank'))
 
 @php
-    $widgets['before_content'][] = [
-        'type'        => 'jumbotron',
-        'heading'     => 'Willkommen!',
-        'content'     => 'Hier entsteht ein Interface für Infinity V RP, um die Arbeit für Supporter an der DB zu erleichtern!',
-    ];
+    $users = DB::table('users')->count();
+
+        $widgets['before_content'][] = [
+            'type'        => 'jumbotron',
+            'heading'     => 'Willkommen!',
+            'content'     => 'Hier entsteht ein Interface für Infinity V RP, um die Arbeit für Supporter an der DB zu erleichtern!',
+        ];
+        $widgets['content'][] = [
+            'type'       => 'card',
+            'content'    => [
+                'header' => 'Anzahl Userdatensätze in der DB',
+                'body'   => $users,
+            ]
+        ];
 @endphp
 
 @section('content')
