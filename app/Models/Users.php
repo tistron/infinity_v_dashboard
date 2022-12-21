9eec5a4e35f9b87a9d3e3ae6e94f8883222637e1
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 19:02:11 +0100

    Update Users.php

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Users extends Model
 {
     use CrudTrait;
+    use Illuminate\Support\Facades\DB;
 
     /*
     |--------------------------------------------------------------------------
