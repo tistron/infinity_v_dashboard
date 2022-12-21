4051286f7cc44cc6dd9e6b45d4b2a613785b0961
Tim Strößner <ts@fp.de>
2022-12-21 15:54:37 +0100

    URL für ACC Data weg

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,4 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('addon-account-data') }}"><i class="nav-icon la la-th-list"></i> Addon account datas</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('users') }}"><i class="nav-icon la la-th-list"></i> Users</a></li>
