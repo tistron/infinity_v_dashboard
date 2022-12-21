05d7d56f4ed7a5370ae834e4c44ed3565176d373
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 18:52:00 +0100

    Update dashboard.blade.php

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -14,7 +14,7 @@
         'type'       => 'card',
         'content'    => [
             'header' => 'Anzahl der Userdatensätze in der DB',
-            'body'   => $user_count,
+            'body'   => $user_count[0],
         ]
     ];
 
