46a8e6e51cd264aa18c28fe09672cd38c6be063b
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 21:23:14 +0100

    Update sidebar_content.blade.php

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,9 +1,9 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('users') }}"><i class="nav-icon la la-th-list"></i>User</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('users') }}"><i class="nav-icon la la-th-list"></i> User</a></li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('owned-vehicles') }}"><i class="nav-icon la la-th-list">Fahrzeuge</i></a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('owned-vehicles') }}"><i class="nav-icon la la-th-list"></i> Fahrzeuge</a></li>
 
 <!-- Users, Roles, Permissions -->
 @php
