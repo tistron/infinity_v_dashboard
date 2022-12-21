0138d271efa98a4e92361f636a5a194ce53c2191
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 19:50:15 +0100

    Update dashboard.blade.php

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -15,6 +15,11 @@
     $instagram_posts = DB::connection('mysql_fivem')->table('instagram_posts')->count();
     $billing_count = DB::connection('mysql_fivem')->table('okokbilling')->count();
     $phone_messages = DB::connection('mysql_fivem')->table('phone_messages')->count();
+    $player_contacts = DB::connection('mysql_fivem')->table('player_contacts')->count();
+    $player_houses = DB::connection('mysql_fivem')->table('player_houses')->count();
+    $player_notes = DB::connection('mysql_fivem')->table('player_notes')->count();
+    $saved_documents = DB::connection('mysql_fivem')->table('saved_documents')->count();
+
 
     $user_count_card_widget_definition = [
         'type'    => 'div',
@@ -25,6 +30,10 @@
             [ 'type' => 'card', 'content' => ['header' => 'Anzahl Instagram Storys', 'body' => $instagram_posts] ],
             [ 'type' => 'card', 'content' => ['header' => 'Anzahl Rechnungen', 'body' => $billing_count] ],
             [ 'type' => 'card', 'content' => ['header' => 'Anzahl Smartphone Nachrichten', 'body' => $phone_messages] ],
+            [ 'type' => 'card', 'content' => ['header' => 'Anzahl Smartphone Kontakte', 'body' => $player_contacts] ],
+            [ 'type' => 'card', 'content' => ['header' => 'Anzahl Häuser', 'body' => $player_houses] ],
+            [ 'type' => 'card', 'content' => ['header' => 'Anzahl Notizen', 'body' => $player_notes] ],
+            [ 'type' => 'card', 'content' => ['header' => 'Anzahl gespeicherte Dokumente', 'body' => $saved_documents] ],
         ]
     ];
     Widget::add($user_count_card_widget_definition)->to('after_content');
