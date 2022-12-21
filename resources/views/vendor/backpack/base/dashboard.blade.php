dd1de6a8f4f0c0c0c01b13793b64094725e962bd
EpixXx <35959732+EpixXx@users.noreply.github.com>
2022-12-21 19:46:56 +0100

    Update dashboard.blade.php

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -7,14 +7,14 @@
     $widgets['before_content'][] = [
         'type'        => 'jumbotron',
         'heading'     => 'Willkommen!',
-        'content'     => 'Hier entsteht ein Interface für Infinity V RP, um die Arbeit für Supporter an der DB zu erleichtern!',
+        'content'     => 'In der Leiste links kannst du auf die Verschiedenen Tabellen zugreifen und diese bei Bedarf anpassen. Viel Spaß!',
     ];
-
 
     $user_count = DB::connection('mysql_fivem')->table('users')->count();
     $vehicle_count = DB::connection('mysql_fivem')->table('owned_vehicles')->count();
     $instagram_posts = DB::connection('mysql_fivem')->table('instagram_posts')->count();
     $billing_count = DB::connection('mysql_fivem')->table('okokbilling')->count();
+    $phone_messages = DB::connection('mysql_fivem')->table('phone_messages')->count();
 
     $user_count_card_widget_definition = [
         'type'    => 'div',
@@ -24,6 +24,7 @@
             [ 'type' => 'card', 'content' => ['header' => 'Anzahl Fahrzeuge', 'body' => $vehicle_count] ],
             [ 'type' => 'card', 'content' => ['header' => 'Anzahl Instagram Storys', 'body' => $instagram_posts] ],
             [ 'type' => 'card', 'content' => ['header' => 'Anzahl Rechnungen', 'body' => $billing_count] ],
+            [ 'type' => 'card', 'content' => ['header' => 'Anzahl Smartphone Nachrichten', 'body' => $phone_messages] ],
         ]
     ];
     Widget::add($user_count_card_widget_definition)->to('after_content');
