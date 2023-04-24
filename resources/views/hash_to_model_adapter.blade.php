99c7b38061d2bddcf7d25faab9a3ebcb30ac38d7
EpixXx <35959732+EpixXx@users.noreply.github.com>
2023-04-24 14:08:13 +0200

    Update hash_to_model_adapter.blade.php

--- resources/views/hash_to_model_adapter.blade.php
+++ resources/views/hash_to_model_adapter.blade.php
@@ -1,34 +1,20 @@
 @extends(backpack_view('blank'))
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Meine Seite</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-</head>
-<body>
-<form>
-    <label for="text-input">Text eingeben:</label>
-    <input type="text" id="text-input" name="text-input">
-    <button type="button" id="submit-button">Absenden</button>
-</form>
+@php
+    $html = '<button type="button" class="btn btn-primary">Click me!</button>';
 
-<script>
-    $(document).ready(function() {
-        $('#submit-button').click(function() {
-            var textInput = $('#text-input').val();
-            $.ajax({
-                type: "POST",
-                url: "/submit",
-                data: {
-                    textInput: textInput
-                },
-                success: function(response) {
-                    console.log(response);
-                }
-            });
-        });
-    });
-</script>
-</body>
-</html>
+    $user_count_card_widget_definition = [
+        'type'    => 'div',
+        'class'   => 'row',
+        'content' => [
+            [ 'type' => 'card', 'content' => ['header' => 'Anzahl User', 'body' => $html ] ],
+        ]
+    ];
+    Widget::add($user_count_card_widget_definition)->to('after_content');
+@endphp
+
+<style>
+</style>
+
+@section('content')
+@endsection
