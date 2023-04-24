a049f43caa6c6deef851e4017c81df217a6eb7da
EpixXx <35959732+EpixXx@users.noreply.github.com>
2023-04-24 13:44:19 +0200

    new hash model page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hash_to_model_adapter', function () {
+    return view('hash_to_model_adapter');
+});
