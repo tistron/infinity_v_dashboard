afb85e362f0aaccfb24a0e3ea8a0d4c2efc1b3c7
EpixXx <35959732+EpixXx@users.noreply.github.com>
2023-04-24 14:21:52 +0200

    Update hash_to_model_adapter.blade.php

--- resources/views/hash_to_model_adapter.blade.php
+++ resources/views/hash_to_model_adapter.blade.php
@@ -1,18 +1,41 @@
 @extends(backpack_view('blank'))
 
 @php
-    $text_html = '<input type="text" name="hash_code" placeholder="Hash Code">';
-    $button_html = '<hr><button type="button" class="btn btn-primary">Model berechnen</button>';
+    $text_html = '<input type="text" id="hash_code" name="hash_code" placeholder="Hash Code">';
+    $button_html = '<button type="button" id="calculate-btn" class="btn btn-primary">Model berechnen</button>';
+    $erg_html = '<div id="result"></div>';
 
     $user_count_card_widget_definition = [
         'type'    => 'div',
         'class'   => 'row',
         'content' => [
-            [ 'type' => 'card', 'content' => ['header' => 'Anzahl User', 'body' => $text_html . $button_html ] ],
+            [ 'type' => 'card', 'content' => ['header' => 'Hash To Model Converter', 'body' => $text_html . $button_html . $erg_html ] ],
         ]
     ];
     Widget::add($user_count_card_widget_definition)->to('after_content');
 @endphp
+
+<script>
+    function calculateModel() {
+        var hashCode = document.getElementById('hash_code').value;
+        jenkinsOneAtATimeHashToString(hashCode).then(function(result) {
+            document.getElementById('result').innerHTML = result;
+        });
+    }
+    document.getElementById('calculate-btn').addEventListener('click', calculateModel);
+
+    function jenkinsOneAtATimeHashToString(hash) {
+        var str = '';
+        for (var i = 0; i < hash.length; i += 2) {
+            var byte = parseInt(hash.substr(i, 2), 16);
+            if (byte === 0) {
+                break;
+            }
+            str += String.fromCharCode(byte);
+        }
+        return str;
+    }
+</script>
 
 <style>
 </style>
