3fb1ca4631c4e515203957ca40b6246d68ad051e
EpixXx <35959732+EpixXx@users.noreply.github.com>
2023-04-24 14:10:57 +0200

    Update hash_to_model_adapter.blade.php

--- resources/views/hash_to_model_adapter.blade.php
+++ resources/views/hash_to_model_adapter.blade.php
@@ -1,7 +1,8 @@
 @extends(backpack_view('blank'))
 
 @php
-    $html = '<input type="text" name="hash_code" placeholder="Hash Code">';
+    $text_html = '<input type="text" name="hash_code" placeholder="Hash Code">';
+    $button_html = '<hr><button type="button" class="btn btn-primary">Model berechnen</button>';
 
     $user_count_card_widget_definition = [
         'type'    => 'div',
