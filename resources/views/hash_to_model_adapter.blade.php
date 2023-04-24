1cdd524c31c60a659d64fb379b0f3e69c4721889
EpixXx <35959732+EpixXx@users.noreply.github.com>
2023-04-24 14:03:34 +0200

    Update hash_to_model_adapter.blade.php

--- resources/views/hash_to_model_adapter.blade.php
+++ resources/views/hash_to_model_adapter.blade.php
@@ -1,12 +1,34 @@
 @extends(backpack_view('blank'))
 
-@php
-    {{ Form::input('text', 'name'); }}
-    {{ Form::input('email', 'email_address', null, ['class' => 'emailfld']); }}
-@endphp
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meine Seite</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+<form>
+    <label for="text-input">Text eingeben:</label>
+    <input type="text" id="text-input" name="text-input">
+    <button type="button" id="submit-button">Absenden</button>
+</form>
 
-<style>
-</style>
-
-@section('content')
-@endsection
+<script>
+    $(document).ready(function() {
+        $('#submit-button').click(function() {
+            var textInput = $('#text-input').val();
+            $.ajax({
+                type: "POST",
+                url: "/submit",
+                data: {
+                    textInput: textInput
+                },
+                success: function(response) {
+                    console.log(response);
+                }
+            });
+        });
+    });
+</script>
+</body>
+</html>
