a68ee6c29b853054a25fd030a93a7176b7aa5643
EpixXx <35959732+EpixXx@users.noreply.github.com>
2023-04-24 13:56:14 +0200

    Update hash_to_model_adapter.blade.php

--- resources/views/hash_to_model_adapter.blade.php
+++ resources/views/hash_to_model_adapter.blade.php
@@ -1,1 +1,14 @@
 @extends(backpack_view('blank'))
+
+@php
+    echo 'Hashwert';
+    echo Form::open(array('url'=>'/support/hash_to_model_adapter'));
+    echo Form::text('hash');
+    echo Form::close();
+@endphp
+
+<style>
+</style>
+
+@section('content')
+@endsection
