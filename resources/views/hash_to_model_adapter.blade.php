a9026eb0f493e3c82d6aca613b82544c0d2628c6
EpixXx <35959732+EpixXx@users.noreply.github.com>
2023-04-24 14:48:30 +0200

    Update hash_to_model_adapter.blade.php

--- resources/views/hash_to_model_adapter.blade.php
+++ resources/views/hash_to_model_adapter.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $text_html = '<input type="text" id="hash_code" name="hash_code" placeholder="Hash Code">';
-    $button_html = '<button type="button" id="calculate-btn" class="btn btn-primary">Model berechnen</button>';
+    $button_html = '<hr><button type="button" id="calculate-btn" class="btn btn-primary">Model berechnen</button><hr>';
     $erg_html = '<div id="result"></div>';
 
     $user_count_card_widget_definition = [
@@ -16,50 +16,6 @@
 @endphp
 
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        document.getElementById('calculate-btn').addEventListener('click', calculateModel);
-    });
-
-    async function calculateModel() {
-        var input = document.getElementById('hash_code').value;
-        var num = parseInt(input);
-
-        if (isNaN(num)) {
-            document.getElementById('result').innerHTML = 'Invalid input';
-            return;
-        }
-
-        var hash = await intToJenkinsHash(Math.abs(num));
-        var str = hashToModelName(hash);
-
-        document.getElementById('result').innerHTML = str;
-    }
-
-    async function intToJenkinsHash(num) {
-        let hash = BigInt(num < 0 ? num + 0x100000000 : num).toString(16);
-        while (hash.length < 8) {
-            hash = "0" + hash;
-        }
-        const bytes = [];
-        for (let i = 0; i < hash.length; i += 2) {
-            const byte = parseInt(hash.substr(i, 2), 16);
-            bytes.push(byte);
-        }
-        const result = await jenkinsOneAtATimeHashToString(bytes);
-        return result;
-    }
-
-    function hashToModelName(hash) {
-        var str = '';
-        for (var i = 0; i < hash.length; i += 2) {
-            var byte = parseInt(hash.substr(i, 2), 16);
-            if (byte === 0) {
-                break;
-            }
-            str += String.fromCharCode(byte);
-        }
-        return str;
-    }
 </script>
 
 <style>
